189ee824c5162e4997af3a64ea7efa5a97298d66
Deepanshukesarwani <deepanshukesarwani7@gmail.com>
2023-04-05 09:13:51 +0530

    added thankupage working pending

---
+++ src/component/Thankupage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './thankupage.css'
+const Thankupage = () => {
+  return (
+    <div>
+      <div className="thankupagewrapper">
+        <h1>hello jee</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Thankupage
